0792581e8a0673d0937d8b7ee640f7a64fa39e42
SheltyDog <hiroki.ishiguro.fv@renesas.com>
2019-08-15 14:37:33 +0900

    [IMPLEMENTED] aws_ota_pal.c for RX65N RSK as on the fly. (excluding prvPAL_WriteBlock() implementation)
    Signed-off-by: SheltyDog <hiroki.ishiguro.fv@renesas.com>

--- lib/ota/portable/renesas/rx65n-rsk/type3/aws_ota_pal.c
+++ lib/ota/portable/renesas/rx65n-rsk/type3/aws_ota_pal.c
@@ -26,14 +26,20 @@
 /* C Runtime includes. */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Amazon FreeRTOS include. */
 #include "FreeRTOS.h"
 #include "aws_ota_pal.h"
 #include "aws_ota_agent_internal.h"
 
+/* Renesas RX Driver Package include */
+#include "platform.h"
+#include "r_flash_rx_if.h"
+
 /* Specify the OTA signature algorithm we support on this platform. */
-const char cOTA_JSON_FileSignatureKey[ OTA_FILE_SIG_KEY_STR_MAX_LENGTH ] = "sig-sha256-ecdsa";   /* FIX ME. */
+//const char cOTA_JSON_FileSignatureKey[ OTA_FILE_SIG_KEY_STR_MAX_LENGTH ] = "sig-sha256-ecdsa";   /* FIX ME. */
+const char cOTA_JSON_FileSignatureKey[ OTA_FILE_SIG_KEY_STR_MAX_LENGTH ] = "sig-sha1-rsa";   /* for Renesas implementation */
 
 
 /* The static functions below (prvPAL_CheckFileSignature and prvPAL_ReadAndAssumeCertificate) 
@@ -49,7 +55,7 @@ const char cOTA_JSON_FileSignatureKey[ OTA_FILE_SIG_KEY_STR_MAX_LENGTH ] = "sig-
  * This function should be implemented if signature verification is not offloaded
  * to non-volatile memory io functions.
  * 
- * This function is called from prvPAL_Close(). 
+ * This function is called\r\n from prvPAL_Close().
  * 
  * @param[in] C OTA file context information.
  * 
@@ -66,7 +72,7 @@ static OTA_Err_t prvPAL_CheckFileSignature( OTA_FileContext_t * const C );
  * 
  * The allocated memory returned becomes the property of the caller who is responsible for freeing it.
  * 
- * This function is called from prvPAL_CheckFileSignature(). It should be implemented if signature
+ * This function is called\r\n from prvPAL_CheckFileSignature(). It should be implemented if signature
  * verification is not offloaded to non-volatile memory io function.
  * 
  * @param[in] pucCertName The file path of the certificate file.
@@ -78,23 +84,155 @@ static OTA_Err_t prvPAL_CheckFileSignature( OTA_FileContext_t * const C );
 static uint8_t * prvPAL_ReadAndAssumeCertificate( const uint8_t * const pucCertName,
                                                   uint32_t * const ulSignerCertSize );
 
+/* Renesas specific implementation data */
+#define LIFECYCLE_STATE_BLANK             (0)
+#define LIFECYCLE_STATE_ON_THE_MARKET     (1)
+#define LIFECYCLE_STATE_UPDATING	      (2)
+
+/*------------------------------------------ firmware update configuration (start) --------------------------------------------*/
+/* R_FLASH_Write() arguments: specify "low address" and process to "high address" */
+#define BOOT_LOADER_LOW_ADDRESS FLASH_CF_BLOCK_13
+#define BOOT_LOADER_MIRROR_LOW_ADDRESS FLASH_CF_BLOCK_51
+
+/* R_FLASH_Erase() arguments: specify "high address (low block number)" and process to "low address (high block number)" */
+#define BOOT_LOADER_MIRROR_HIGH_ADDRESS FLASH_CF_BLOCK_38
+#define BOOT_LOADER_UPDATE_TEMPORARY_AREA_HIGH_ADDRESS FLASH_CF_BLOCK_52
+
+#define BOOT_LOADER_MIRROR_BLOCK_NUM_FOR_SMALL 8
+#define BOOT_LOADER_MIRROR_BLOCK_NUM_FOR_MEDIUM 6
+
+#define FLASH_INTERRUPT_PRIORITY 15	/* 0(low) - 15(high) */
+/*------------------------------------------ firmware update configuration (end) --------------------------------------------*/
+
+#define BOOT_LOADER_UPDATE_TEMPORARY_AREA_LOW_ADDRESS FLASH_CF_LO_BANK_LO_ADDR
+#define BOOT_LOADER_UPDATE_EXECUTE_AREA_LOW_ADDRESS FLASH_CF_HI_BANK_LO_ADDR
+#define BOOT_LOADER_UPDATE_TARGET_BLOCK_NUMBER (FLASH_NUM_BLOCKS_CF - BOOT_LOADER_MIRROR_BLOCK_NUM_FOR_SMALL - BOOT_LOADER_MIRROR_BLOCK_NUM_FOR_MEDIUM)
+
+#define SHA1_HASH_LENGTH_BYTE_SIZE 20
+
+#define FIRMWARE_UPDATE_STATE_CLOSED 0
+#define FIRMWARE_UPDATE_STATE_INITIALIZED 1
+#define FIRMWARE_UPDATE_STATE_ERASE 2
+#define FIRMWARE_UPDATE_STATE_ERASE_WAIT_COMPLETE 3
+#define FIRMWARE_UPDATE_STATE_READ_WAIT_COMPLETE 4
+#define FIRMWARE_UPDATE_STATE_WRITE_WAIT_COMPLETE 5
+#define FIRMWARE_UPDATE_STATE_FINALIZE 6
+#define FIRMWARE_UPDATE_STATE_FINALIZE_WAIT_ERASE_DATA_FLASH 7
+#define FIRMWARE_UPDATE_STATE_FINALIZE_WAIT_WRITE_DATA_FLASH 8
+#define FIRMWARE_UPDATE_STATE_FINALIZE_COMPLETED 9
+#define FIRMWARE_UPDATE_STATE_CAN_SWAP_BANK 100
+#define FIRMWARE_UPDATE_STATE_WAIT_START 101
+#define FIRMWARE_UPDATE_STATE_COMPLETED 102
+#define FIRMWARE_UPDATE_STATE_ERROR 103
+#define FIRMWARE_UPDATE_STATE_WRITE_COMPLETE 201
+#define FIRMWARE_UPDATE_STATE_ERASE_COMPLETE 202
+
+typedef struct _load_firmware_control_block {
+	uint32_t status;
+	uint8_t *flash_buffer_for_erase_unit;
+	uint8_t *flash_buffer_program_unit;
+	uint32_t offset;
+	volatile uint32_t flash_write_in_progress_flag;
+	volatile uint32_t flash_erase_in_progress_flag;
+	uint32_t progress;
+	uint8_t hash_sha1[SHA1_HASH_LENGTH_BYTE_SIZE];
+	uint32_t firmware_length;
+	OTA_ImageState_t eSavedAgentState;
+	OTA_FileContext_t OTA_FileContext;
+}LOAD_FIRMWARE_CONTROL_BLOCK;
+
+typedef struct _firmware_update_control_block_sub
+{
+	uint32_t user_program_max_cnt;
+	uint32_t lifecycle_state;
+	uint8_t hash0_sha1[SHA1_HASH_LENGTH_BYTE_SIZE];
+	uint8_t hash1_sha1[SHA1_HASH_LENGTH_BYTE_SIZE];
+}FIRMWARE_UPDATE_CONTROL_BLOCK_SUB;
+
+typedef struct _firmware_update_control_block
+{
+	FIRMWARE_UPDATE_CONTROL_BLOCK_SUB data;
+	uint8_t hash_sha1[SHA1_HASH_LENGTH_BYTE_SIZE];
+}FIRMWARE_UPDATE_CONTROL_BLOCK;
+
+LOAD_FIRMWARE_CONTROL_BLOCK load_firmware_control_block;
+
+static void bank_swap(void);
+static uint32_t *flash_copy_vector_table(void);
+static void dummy_int(void);
+
 /*-----------------------------------------------------------*/
 
 OTA_Err_t prvPAL_CreateFileForRx( OTA_FileContext_t * const C )
 {
     DEFINE_OTA_METHOD_NAME( "prvPAL_CreateFileForRx" );
+    OTA_LOG_L1("[%s] is called.\r\n", OTA_METHOD_NAME);
+    OTA_Err_t eResult = kOTA_Err_Uninitialized; /* For MISRA mandatory. */
+    static uint8_t *dummy_file_pointer;
+    static uint8_t dummy_file;
+    dummy_file_pointer = &dummy_file;
 
-    /* FIX ME. */
-    return kOTA_Err_RxFileCreateFailed;
+    if( C != NULL )
+    {
+        if( C->pucFilePath != NULL )
+        {
+        	if((load_firmware_control_block.status != FIRMWARE_UPDATE_STATE_CLOSED)
+        			|| (load_firmware_control_block.status != FIRMWARE_UPDATE_STATE_FINALIZE_COMPLETED)
+        			|| (load_firmware_control_block.status != FIRMWARE_UPDATE_STATE_COMPLETED)
+        			|| (load_firmware_control_block.status != FIRMWARE_UPDATE_STATE_ERROR))
+        	{
+				memset(&load_firmware_control_block, 0, sizeof(LOAD_FIRMWARE_CONTROL_BLOCK));
+				load_firmware_control_block.status = FIRMWARE_UPDATE_STATE_INITIALIZED;
+				load_firmware_control_block.eSavedAgentState = eOTA_ImageState_Unknown;
+				memcpy(&load_firmware_control_block.OTA_FileContext, C, sizeof(OTA_FileContext_t));
+				C->pucFile = dummy_file_pointer;
+				eResult = kOTA_Err_None;
+				OTA_LOG_L1( "[%s] Receive file created.\r\n", OTA_METHOD_NAME );
+        	}
+        	else
+        	{
+                eResult = kOTA_Err_RxFileCreateFailed;
+                OTA_LOG_L1( "[%s] ERROR - now in progress.\r\n", OTA_METHOD_NAME );
+        	}
+        }
+        else
+        {
+            eResult = kOTA_Err_RxFileCreateFailed;
+            OTA_LOG_L1( "[%s] ERROR - Invalid context provided.\r\n", OTA_METHOD_NAME );
+        }
+    }
+    else
+    {
+        eResult = kOTA_Err_RxFileCreateFailed;
+        OTA_LOG_L1( "[%s] ERROR - Invalid context provided.\r\n", OTA_METHOD_NAME );
+    }
+
+    return eResult;
 }
+
 /*-----------------------------------------------------------*/
 
 OTA_Err_t prvPAL_Abort( OTA_FileContext_t * const C )
 {
     DEFINE_OTA_METHOD_NAME( "prvPAL_Abort" );
-    
-    /* FIX ME. */
-    return kOTA_Err_FileAbort;
+    OTA_LOG_L1("[%s] is called.\r\n", OTA_METHOD_NAME);
+
+    /* Set default return status to uninitialized. */
+    OTA_Err_t eResult = kOTA_Err_Uninitialized;
+
+    if( NULL != C )
+    {
+		memset(&load_firmware_control_block, 0, sizeof(LOAD_FIRMWARE_CONTROL_BLOCK));
+		load_firmware_control_block.status = FIRMWARE_UPDATE_STATE_ERROR;
+        OTA_LOG_L1( "[%s] OK\r\n", OTA_METHOD_NAME );
+        eResult = kOTA_Err_None;
+    }
+    else /* Context was not valid. */
+    {
+        OTA_LOG_L1( "[%s] ERROR - Invalid context.\r\n", OTA_METHOD_NAME );
+        eResult = kOTA_Err_FileAbort;
+    }
+    return eResult;
 }
 /*-----------------------------------------------------------*/
 
@@ -105,18 +243,43 @@ int16_t prvPAL_WriteBlock( OTA_FileContext_t * const C,
                            uint32_t ulBlockSize )
 {
     DEFINE_OTA_METHOD_NAME( "prvPAL_WriteBlock" );
+    OTA_LOG_L1("[%s] is called.\r\n", OTA_METHOD_NAME);
 
-    /* FIX ME. */
-    return -1;
+    /* Set default return status to uninitialized. */
+    OTA_Err_t eResult = kOTA_Err_Uninitialized;
+
+    eResult = kOTA_Err_None;
+    return eResult;
 }
 /*-----------------------------------------------------------*/
 
 OTA_Err_t prvPAL_CloseFile( OTA_FileContext_t * const C )
 {
     DEFINE_OTA_METHOD_NAME( "prvPAL_CloseFile" );
+    OTA_LOG_L1("[%s] is called.\r\n", OTA_METHOD_NAME);
 
-    /* FIX ME. */
-    return kOTA_Err_FileClose;
+    OTA_Err_t eResult = kOTA_Err_None;
+    if( NULL != C )
+    {
+    	if(load_firmware_control_block.status != FIRMWARE_UPDATE_STATE_FINALIZE_COMPLETED)
+    	{
+			memset(&load_firmware_control_block, 0, sizeof(LOAD_FIRMWARE_CONTROL_BLOCK));
+			load_firmware_control_block.status = FIRMWARE_UPDATE_STATE_CLOSED;
+			OTA_LOG_L1( "[%s] OK\r\n", OTA_METHOD_NAME );
+			eResult = kOTA_Err_None;
+    	}
+		else
+		{
+			OTA_LOG_L1( "[%s] ERROR - Invalid status.\r\n", OTA_METHOD_NAME );
+			eResult = kOTA_Err_FileClose;
+		}
+    }
+    else /* Context was not valid. */
+    {
+        OTA_LOG_L1( "[%s] ERROR - Invalid context.\r\n", OTA_METHOD_NAME );
+        eResult = kOTA_Err_FileAbort;
+    }
+    return eResult;
 }
 /*-----------------------------------------------------------*/
 
@@ -124,8 +287,9 @@ OTA_Err_t prvPAL_CloseFile( OTA_FileContext_t * const C )
 static OTA_Err_t prvPAL_CheckFileSignature( OTA_FileContext_t * const C )
 {
     DEFINE_OTA_METHOD_NAME( "prvPAL_CheckFileSignature" );
+    OTA_LOG_L1("[%s] is called.\r\n", OTA_METHOD_NAME);
 
-    /* FIX ME. */
+    /* not implemented */
     return kOTA_Err_SignatureCheckFailed;
 }
 /*-----------------------------------------------------------*/
@@ -134,8 +298,9 @@ static uint8_t * prvPAL_ReadAndAssumeCertificate( const uint8_t * const pucCertN
                                                   uint32_t * const ulSignerCertSize )
 {
     DEFINE_OTA_METHOD_NAME( "prvPAL_ReadAndAssumeCertificate" );
+    OTA_LOG_L1("[%s] is called.\r\n", OTA_METHOD_NAME);
 
-    /* FIX ME. */
+    /* not implemented */
     return NULL;
 }
 /*-----------------------------------------------------------*/
@@ -143,9 +308,9 @@ static uint8_t * prvPAL_ReadAndAssumeCertificate( const uint8_t * const pucCertN
 OTA_Err_t prvPAL_ResetDevice( void )
 {
     DEFINE_OTA_METHOD_NAME("prvPAL_ResetDevice");
+    OTA_LOG_L1("[%s] is called.\r\n", OTA_METHOD_NAME);
 
-    /* FIX ME. */
-    return kOTA_Err_ResetNotSupported;
+    return kOTA_Err_None;
 }
 /*-----------------------------------------------------------*/
 
@@ -153,26 +318,69 @@ OTA_Err_t prvPAL_ActivateNewImage( void )
 {
     DEFINE_OTA_METHOD_NAME("prvPAL_ActivateNewImage");
 
-    /* FIX ME. */
-    return kOTA_Err_Uninitialized;
+    OTA_LOG_L1( "[%s] Changing the Startup Bank\r\n", OTA_METHOD_NAME );
+
+    /* Bank swap processing */
+	bank_swap();
+    while(1);
+
+    /* We shouldn't actually get here if the board supports the auto reset.
+     * But, it doesn't hurt anything if we do although someone will need to
+     * reset the device for the new image to boot. */
+
+    return kOTA_Err_None;
 }
 /*-----------------------------------------------------------*/
 
 OTA_Err_t prvPAL_SetPlatformImageState( OTA_ImageState_t eState )
 {
     DEFINE_OTA_METHOD_NAME( "prvPAL_SetPlatformImageState" );
+    OTA_LOG_L1("[%s] is called.\r\n", OTA_METHOD_NAME);
 
-    /* FIX ME. */
-    return kOTA_Err_BadImageState;
+    OTA_Err_t eResult = kOTA_Err_None;
+
+    if( eState != eOTA_ImageState_Unknown && eState <= eOTA_LastImageState )
+    {
+    	/* Save the image state to eSavedAgentState. */
+    	load_firmware_control_block.eSavedAgentState = eState;
+    }
+    else /* Image state invalid. */
+    {
+        OTA_LOG_L1( "[%s] ERROR - Invalid image state provided.\r\n", OTA_METHOD_NAME );
+        eResult = kOTA_Err_BadImageState;
+    }
+
+    return eResult;
 }
 /*-----------------------------------------------------------*/
 
 OTA_PAL_ImageState_t prvPAL_GetPlatformImageState( void )
 {
     DEFINE_OTA_METHOD_NAME( "prvPAL_GetPlatformImageState" );
+    OTA_LOG_L1("[%s] is called.\r\n", OTA_METHOD_NAME);
 
-    /* FIX ME. */
-    return eOTA_ImageState_Unknown;
+	OTA_PAL_ImageState_t ePalState = eOTA_PAL_ImageState_Unknown;
+
+	switch (load_firmware_control_block.eSavedAgentState)
+	{
+		case eOTA_ImageState_Testing:
+			ePalState = eOTA_PAL_ImageState_PendingCommit;
+			break;
+		case eOTA_ImageState_Accepted:
+			ePalState = eOTA_PAL_ImageState_Valid;
+			break;
+		case eOTA_ImageState_Unknown: // Uninitialized image state, assume a factory image
+			ePalState = eOTA_PAL_ImageState_Valid;
+			break;
+		case eOTA_ImageState_Rejected:
+		case eOTA_ImageState_Aborted:
+		default:
+			ePalState = eOTA_PAL_ImageState_Invalid;
+			break;
+	}
+
+	OTA_LOG_L1("Function call: prvPAL_GetPlatformImageState: [%d]\r\n", ePalState);
+    return ePalState; /*lint !e64 !e480 !e481 I/O calls and return type are used per design. */
 }
 /*-----------------------------------------------------------*/
 
@@ -180,3 +388,67 @@ OTA_PAL_ImageState_t prvPAL_GetPlatformImageState( void )
 #ifdef AMAZON_FREERTOS_ENABLE_UNIT_TESTS
     #include "aws_ota_pal_test_access_define.h"
 #endif
+
+/******************************************************************************
+ Function Name   : bank_swap()
+ Description     :
+ Arguments       : none
+ Return value    : none
+ ******************************************************************************/
+void bank_swap(void)
+{
+	flash_err_t flash_err;
+	if(FIRMWARE_UPDATE_STATE_CAN_SWAP_BANK == load_firmware_control_block.status)
+	{
+		R_BSP_CpuInterruptLevelWrite(FLASH_INTERRUPT_PRIORITY - 1);
+		flash_copy_vector_table();
+		flash_err = R_FLASH_Control(FLASH_CMD_BANK_TOGGLE, NULL);
+		if(FLASH_SUCCESS == flash_err)
+		{
+			while(1);	/* wait software reset in RAM */
+		}
+		while(1); /* death loop */
+	}
+}
+
+/***********************************************************************************************************************
+* Function Name: flash_copy_vector_table
+* Description  : Moves the relocatable vector table to RAM. This is only needed if ROM operations are performed.
+*                ROM cannot be accessed during operations. The vector table is located in ROM and will be accessed
+*                if an interrupt occurs.
+* Arguments    : none
+* Return Value : uint32_t *flash_vect
+***********************************************************************************************************************/
+static uint32_t *flash_copy_vector_table(void)
+{
+    uint32_t *flash_vect;
+    uint32_t   i;
+    static uint32_t g_ram_vector_table[256];      // RAM space to hold the vector table
+
+    /* Get address of variable vector table in ROM */
+    flash_vect = (uint32_t *)get_intb();
+
+    /* Copy over variable vector table to RAM */
+    for(i = 0; i < 256; i++ )
+    {
+    	g_ram_vector_table[i] = (uint32_t)dummy_int;      // Copy over entry
+    }
+
+    g_ram_vector_table[VECT_FCU_FIFERR] = flash_vect[VECT_FCU_FIFERR];
+    g_ram_vector_table[VECT_FCU_FRDYI] = flash_vect[VECT_FCU_FRDYI];
+
+    /* Set INTB to ram address */
+#if __RENESAS_VERSION__ >= 0x01010000
+    set_intb((void *)&g_ram_vector_table[0] );
+#else
+    set_intb( (uint32_t)&g_ram_vector_table[0] );
+#endif
+    return flash_vect;
+}
+
+#pragma section FRAM2
+#pragma interrupt (dummy_int)
+static void dummy_int(void)
+{
+	/* nothing to do */
+}
