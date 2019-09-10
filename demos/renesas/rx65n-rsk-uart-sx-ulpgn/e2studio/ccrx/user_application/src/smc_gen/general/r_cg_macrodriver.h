76ebbaf42c3fea1e9514f688625517d30d3bc6e5
vannam.dinh.xt <vannam.dinh.xt@renesas.com>
2019-09-10 13:52:31 +0900

    [FIXED] RX65N RSK + Silex SX-ULPGN PMOD CC-RX e2 studio with E2 Emulator Lite
    - Changed pin setting
    
    Signed-off-by: vannam.dinh.xt <vannam.dinh.xt@renesas.com>

--- demos/renesas/rx65n-rsk-uart-sx-ulpgn/e2studio/ccrx/user_application/src/smc_gen/general/r_cg_macrodriver.h
+++ demos/renesas/rx65n-rsk-uart-sx-ulpgn/e2studio/ccrx/user_application/src/smc_gen/general/r_cg_macrodriver.h
@@ -22,7 +22,7 @@
 * Version      : 1.2.5
 * Device(s)    : R5F565NEDxFC
 * Description  : Macro header file for code generation.
-* Creation Date: 2019-07-25
+* Creation Date: 2019-09-10
 ***********************************************************************************************************************/
 
 #ifndef MACRODRIVER_H
