435906a60f426e3f5b561c3bcdcccbd820d45e81
vannam.dinh.xt <vannam.dinh.xt@renesas.com>
2019-09-17 16:18:54 +0900

    [FIXED] RX65N RSK esp32 ccrx e2studio E2Lite Project
    Signed-off-by: vannam.dinh.xt <vannam.dinh.xt@renesas.com>

--- demos/renesas/rx65n-rsk-uart-esp32/e2studio/ccrx/user_application/src/smc_gen/general/r_smc_cgc.h
+++ demos/renesas/rx65n-rsk-uart-esp32/e2studio/ccrx/user_application/src/smc_gen/general/r_smc_cgc.h
@@ -22,7 +22,7 @@
 * Version      : 1.4.0
 * Device(s)    : R5F565NEDxFC
 * Description  : This file implements cgc setting.
-* Creation Date: 2019-07-24
+* Creation Date: 2019-09-13
 ***********************************************************************************************************************/
 
 #ifndef SMC_CGC_H
