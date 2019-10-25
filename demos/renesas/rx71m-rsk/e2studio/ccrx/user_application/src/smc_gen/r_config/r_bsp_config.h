0418843a680d22e1b2bda1854266a8e30f15522f
HiroakiHashiguchi <hiroaki.hashiguchi.bx@renesas.com>
2019-10-25 21:42:10 +0900

    [FIXED]RX71M RSK CC-RX e2 studio with E2 Emulator Lite Build error.
    Signed-off-by: HiroakiHashiguchi <hiroaki.hashiguchi.bx@renesas.com>

--- demos/renesas/rx71m-rsk/e2studio/ccrx/user_application/src/smc_gen/r_config/r_bsp_config.h
+++ demos/renesas/rx71m-rsk/e2studio/ccrx/user_application/src/smc_gen/r_config/r_bsp_config.h
@@ -546,5 +546,14 @@ Configuration Options
  */
 #define MY_BSP_CFG_USE_USBIP                        (0)
 
+/* This macro is used to select which SCI bit-rate.
+ */
+#define MY_BSP_CFG_SERIAL_TERM_SCI_BITRATE          (115200)
+
+/* This macro is used to select which SCI interrupt priority.
+   0(low) - 15(high)
+ */
+#define MY_BSP_CFG_SERIAL_TERM_SCI_INTERRUPT_PRIORITY   (15)
+
 #endif /* R_BSP_CONFIG_REF_HEADER_FILE */
 
