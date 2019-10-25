8c7f54b7ed73f4b0771ec87e399e4c35635a592d
HiroakiHashiguchi <hiroaki.hashiguchi.bx@renesas.com>
2019-10-25 21:28:14 +0900

    [FIXED]RX65N Envision Kit CC-RX e2 studio with E2 Emulator Lite (on board) Fixed Build error.
    Signed-off-by: HiroakiHashiguchi <hiroaki.hashiguchi.bx@renesas.com>

--- demos/renesas/rx65n-envision-kit/e2studio/ccrx/user_application/src/smc_gen/r_config/r_bsp_config.h
+++ demos/renesas/rx65n-envision-kit/e2studio/ccrx/user_application/src/smc_gen/r_config/r_bsp_config.h
@@ -607,5 +607,14 @@ Configuration Options
 
 #define MY_BSP_CFG_SERIAL_TERM_SCI                  (2)
 
+/* This macro is used to select which SCI bit-rate.
+ */
+#define MY_BSP_CFG_SERIAL_TERM_SCI_BITRATE          (115200)
+
+/* This macro is used to select which SCI interrupt priority.
+   0(low) - 15(high)
+ */
+#define MY_BSP_CFG_SERIAL_TERM_SCI_INTERRUPT_PRIORITY   (15)
+
 #endif /* R_BSP_CONFIG_REF_HEADER_FILE */
 
