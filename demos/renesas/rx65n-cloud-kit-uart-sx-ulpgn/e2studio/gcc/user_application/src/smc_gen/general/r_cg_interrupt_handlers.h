f45c2df4726d910cb8f8248509b001809e3084ab
vannam.dinh.xt <vannam.dinh.xt@renesas.com>
2019-10-29 15:52:54 +0900

    [FIXED] scfg file
    Signed-off-by: vannam.dinh.xt <vannam.dinh.xt@renesas.com>

--- demos/renesas/rx65n-cloud-kit-uart-sx-ulpgn/e2studio/gcc/user_application/src/smc_gen/general/r_cg_interrupt_handlers.h
+++ demos/renesas/rx65n-cloud-kit-uart-sx-ulpgn/e2studio/gcc/user_application/src/smc_gen/general/r_cg_interrupt_handlers.h
@@ -22,7 +22,7 @@
 * Version      : 1.2.5
 * Device(s)    : R5F565NEDxFP
 * Description  : This file declares interrupt handlers.
-* Creation Date: 2019-08-22
+* Creation Date: 2019-10-29
 ***********************************************************************************************************************/
 
 #ifndef INTERRUPT_HANDLERS_H
