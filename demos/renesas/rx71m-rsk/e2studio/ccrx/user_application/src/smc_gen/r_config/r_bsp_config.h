376ecc0f795076e8d58a4c6de393bdaf13c05240
vannam.dinh.xt <vannam.dinh.xt@renesas.com>
2019-10-30 11:16:28 +0900

    [UPDATED] Updated to BSP v5.40
    Signed-off-by: vannam.dinh.xt <vannam.dinh.xt@renesas.com>

--- demos/renesas/rx71m-rsk/e2studio/ccrx/user_application/src/smc_gen/r_config/r_bsp_config.h
+++ demos/renesas/rx71m-rsk/e2studio/ccrx/user_application/src/smc_gen/r_config/r_bsp_config.h
@@ -54,6 +54,7 @@
 *                              Added the following macro definition.
 *                                - BSP_CFG_FIT_IPL_MAX
 *         : 28.02.2019 3.00    Added support for GNUC and ICCRX.
+*         : 26.07.2019 3.01    Modified comment.
 ***********************************************************************************************************************/
 #ifndef R_BSP_CONFIG_REF_HEADER_FILE
 #define R_BSP_CONFIG_REF_HEADER_FILE
@@ -168,7 +169,7 @@ Configuration Options
    1) Set this macro (BSP_CFG_HEAP_BYTES) to 0.
    2) Set the macro BSP_CFG_IO_LIB_ENABLE to 0.
    3) Disable stdio from being built into the project library. This is done by going into the Renesas RX Toolchain 
-      settings and choosing the Standard Library section. After that choose 'Contents' in E2Studio.
+      settings and choosing the Standard Library section. After that choose 'Contents' in e2 studio.
       This will present a list of modules that can be included. Uncheck the box for stdio.h.
    NOTE: This setting is available only when using CCRX and GNUC. */
 #define BSP_CFG_HEAP_BYTES              (0x1000)
@@ -554,6 +555,5 @@ Configuration Options
    0(low) - 15(high)
  */
 #define MY_BSP_CFG_SERIAL_TERM_SCI_INTERRUPT_PRIORITY   (15)
-
 #endif /* R_BSP_CONFIG_REF_HEADER_FILE */
 
