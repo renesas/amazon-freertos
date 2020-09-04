e170e348b620e36d3ab123e5ddb8882456341a96
duyphan <duy.phan@gnomons.com>
2020-09-04 13:55:43 +0700

    moved SC config files from project folder to vendor folder

--- tools/idt_scripts/flash.bat
+++ tools/idt_scripts/flash.bat
@@ -1,4 +1,4 @@
-del "C:\temp\LOG\Misc\flash.log"
+::del "C:\temp\LOG\Misc\flash.log"
 
 "C:\Program Files (x86)\Renesas Electronics\Programming Tools\Renesas Flash Programmer V3.06\rfpv3.exe" /silent "%1\vendors\renesas\boards\rx72n-envision-kit\aws_tests\flash_project\flash_project.rpj" /file "%1\projects\renesas\rx72n-envision-kit\e2studio\aws_tests\HardwareDebug\aws_tests.mot" /log "C:\temp\LOG\Misc\flash.log"
 ECHO Flashing done!
